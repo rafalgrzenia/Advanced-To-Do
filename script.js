5971b9ad1cea569f09038a6a8a1d8ba6b6bc257e
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-03-17 10:36:26 +0100

    Refactor addToDoItem function

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ const template = document.querySelector("#list-item-template");
 const todoList = document.querySelector("#list");
 const todoTextInput = document.querySelector("#todo-input");
 
+// Functions
 
+function addToDoItem(item) {
+  item.preventDefault();
+  const todoName = todoTextInput.value;
+  const cloneTemplate = template.content.cloneNode(true);
+  const textElement = cloneTemplate.querySelector("[data-list-item-text");
+
+  if (todoName === "") return;
+  textElement.innerText = todoName;
+  todoList.appendChild(cloneTemplate);
+  todoTextInput.value = "";
+}
 
 // Events
 
-addTodoButton.addEventListener("click", e => {
-    e.preventDefault();
-    const cloneTemplate = template.content.cloneNode(true);
-    const textElement = cloneTemplate.querySelector("[data-list-item-text");
-    textElement.innerText = todoTextInput.value;
-    todoList.appendChild(textElement);
-    todoTextInput.value = "";
-})
+addTodoButton.addEventListener("click", addToDoItem);
