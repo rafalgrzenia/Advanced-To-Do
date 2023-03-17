07341c38118fbd8881825866e6e3d52c9f2a977a
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-03-17 12:12:20 +0100

    Fix addToDoItem function

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ const form = document.querySelector("#new-todo-form");
 const template = document.querySelector("#list-item-template");
 const todoList = document.querySelector("#list");
 const todoTextInput = document.querySelector("#todo-input");
+const toDoArray = [];
+
 
 // Functions
 
-function addToDoItem(item) {
-  item.preventDefault();
-  const todoName = todoTextInput.value;
+function addToDoItem(todoName) {
+  
   const cloneTemplate = template.content.cloneNode(true);
   const textElement = cloneTemplate.querySelector("[data-list-item-text");
-
-  if (todoName === "") return;
   textElement.innerText = todoName;
   todoList.appendChild(cloneTemplate);
   todoTextInput.value = "";
